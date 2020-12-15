e1197a388e18db611769d36e13ba261f45783b73
Thomas Donohue <donth77@gmail.com>
2020-12-15 00:17:13 -0800

    Day 15

---
+++ day15/main.cpp
@@ -0,0 +1,33 @@
+#include <vector> 
+#include <iostream>
+#include <unordered_map>
+using namespace std; 
+
+vector<int> nums{ 6, 19, 0, 5, 7, 13, 1 }; 
+
+int speak_sequence(int finalTurn) {
+  unordered_map<int, int> lastSeen;
+  int prev = nums[nums.size() - 1];
+
+  for (int i = 0; i < nums.size(); i++) {
+    lastSeen[nums[i]] = i;
+  }
+
+  for (int i = nums.size(); i < finalTurn; i++) {
+    int curr;
+    if (lastSeen.find(prev) == lastSeen.end()) {
+      curr = 0;
+    } else {
+      curr = i - 1 - lastSeen[prev];
+    }
+    lastSeen[prev] = i - 1;
+    prev = curr;
+  }
+  return prev;
+}
+
+int main() {
+  cout << "Part 1\n" << speak_sequence(2020) << endl;
+  cout << "Part 2\n" << speak_sequence(30000000) << endl; 
+  return 0;
+}
---
+++ day15/main.py
@@ -0,0 +1,27 @@
+import collections
+nums = [6, 19, 0, 5, 7, 13, 1]
+
+
+def speakSequence(finalTurn):
+    numDict = {}
+    for i, num in enumerate(nums):
+        numDict[num] = collections.deque([i], maxlen=2)
+
+    last = nums[-1]
+    for i in range(len(nums), finalTurn):
+        turnsDeque = numDict[last]
+
+        if len(turnsDeque) == 1:
+            spoken = 0
+        else:
+            spoken = turnsDeque[1]-turnsDeque[0]
+        if spoken in numDict:
+            numDict[spoken].append(i)
+        else:
+            numDict[spoken] = collections.deque([i], maxlen=2)
+        last = spoken
+    return spoken
+
+
+print(f'Part 1\n{speakSequence(2020)}')
+print(f'Part 2\n{speakSequence(30000000)}')
